7d73626ffaa4b43232f95158353813566d2504d1
davidu2d <u2dtecnologia@gmail.com>
2023-09-11 10:47:10 -0300

    implementação de pattern repository com injeção de dependencia

--- src/main/java/com/u2d/repository/repository/CustomerRepository.java
+++ src/main/java/com/u2d/repository/repository/CustomerRepository.java
@@ -2,8 +2,6 @@ package com.u2d.repository.repository;
 
 import com.u2d.repository.entity.Customer;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
-public class CustomerRepository implements PanacheRepository<Customer> {
+public interface CustomerRepository extends PanacheRepository<Customer> {
 }
---
+++ src/main/java/com/u2d/repository/repository/impl/CustomerRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.u2d.repository.repository.impl;
+
+import com.u2d.repository.repository.CustomerRepository;
+import jakarta.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class CustomerRepositoryImpl implements CustomerRepository {
+}
